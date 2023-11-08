cdb35ec0029e0fb4b67693e30598a26e9f6c38a8
Millepor21 <millepor21@gmail.com>
2023-11-08 16:38:27 -0500

    added specific all origins cors

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import JWTManager
 from Config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
